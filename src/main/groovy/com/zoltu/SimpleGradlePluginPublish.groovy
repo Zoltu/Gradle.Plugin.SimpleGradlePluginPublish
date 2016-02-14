fdc89302bd13c36daea7710993f90ef46ed3d599
Micah Zoltu <micah@zoltu.net>
2016-02-14 15:35:03 -0800

    Improves error messages.

--- src/main/groovy/com/zoltu/SimpleGradlePluginPublish.groovy
+++ src/main/groovy/com/zoltu/SimpleGradlePluginPublish.groovy
@@ -16,13 +16,13 @@ class SimpleGradlePluginPublish implements Plugin<Project> {
 
 		// migrate configuration to official plugin before publishPlugins runs
 		project.tasks.findByPath("publishPlugins").doFirst { task ->
-			if (project.simpleGradlePluginPublishConfiguration.gradlePublishKey == null) throw new Exception("You must include a gradlePublishKey on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.gradlePublishSecret == null) throw new Exception("You must include a gradlePublishSecret on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.description == null) throw new Exception("You must include a description on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.tags == null) throw new Exception("You must include a tags on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.vcsUrl == null) throw new Exception("You must include a vcsUrl on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.id == null) throw new Exception("You must include a id on simpleGradlePluginPublishConfiguration")
-			if (project.simpleGradlePluginPublishConfiguration.displayName == null) throw new Exception("You must include a displayName on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.gradlePublishKey == null) throw new Exception("You must include a non-null gradlePublishKey on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.gradlePublishSecret == null) throw new Exception("You must include a non-null gradlePublishSecret on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.description == null) throw new Exception("You must include a non-null description on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.tags == null) throw new Exception("You must include a non-null tags on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.vcsUrl == null) throw new Exception("You must include a non-null vcsUrl on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.id == null) throw new Exception("You must include a non-null id on simpleGradlePluginPublishConfiguration")
+			if (project.simpleGradlePluginPublishConfiguration.displayName == null) throw new Exception("You must include a non-null displayName on simpleGradlePluginPublishConfiguration")
 
 			System.setProperty("gradle.publish.key", project.simpleGradlePluginPublishConfiguration.gradlePublishKey)
 			System.setProperty("gradle.publish.secret", project.simpleGradlePluginPublishConfiguration.gradlePublishSecret)
